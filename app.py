48dbdd8bfacbf09e2f72b663e1d24e0aa98aa548
Muhassin <muhassin.k@uniathena.com>
2024-12-22 20:48:50 +0530

    efdcx

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # main.py
 from fastapi import FastAPI
 
-# Create an instance of the FastAPI class
+# Create an instance of the FastAPI class #
 app = FastAPI()
 
 # Define a route for the root endpoint
